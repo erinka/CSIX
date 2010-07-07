7527990bc1df9dac5080abe04175b41f4d896b26
pipikovia <pipikovia@ubuntu.ubuntu-domain>
2010-07-07 22:02:09 +0100

    Added posts system

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -5,18 +5,21 @@ authorization do
   role :admin do
     has_permission_on :home, :about, :to => :index
     has_permission_on :users, :to => [:new, :show, :create, :edit, :update]
+    has_permission_on :posts, :to => [:index, :new, :show, :edit, :destroy, :create, :update]
     has_permission_on :user_sessions, :to => [:new, :create, :destroy, :edit]
   end
   
   role :user do
     has_permission_on :home, :about, :to => :index
     has_permission_on :users, :to => [:new, :show, :create, :edit, :update]
+    has_permission_on :posts, :to => [ :index, :new, :show, :edit, :create, :update]
     has_permission_on :user_sessions, :to => [:new, :create, :destroy, :edit]
   end
   
   role :guest do
     has_permission_on :home, :about, :to => :index
     has_permission_on :users, :to => [:new, :create, :index, :show]
+    has_permission_on :posts, :to => [:show, :index]
     has_permission_on :user_sessions, :to => [:new, :create, :destroy, :edit]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100513074426) do
+ActiveRecord::Schema.define(:version => 20100707203641) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
